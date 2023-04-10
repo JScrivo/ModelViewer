95a2088e72fa7692ff6ca4596ccc0b2c4d2ff07f
scrivjor <scrivjor@sheridancollege.ca>
2023-04-10 15:19:02 -0400

    Created toggle style for AR view toggles

--- ModelViewer/Views/ARViewScreen/Compontents/ModelInteractButton.swift
+++ ModelViewer/Views/ARViewScreen/Compontents/ModelInteractButton.swift
@@ -21,11 +21,41 @@ struct ModelInteractButton: ButtonStyle {
         }
 }
 
+struct ModelInteractToggle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+            .padding(8)
+            .foregroundColor(.black)
+            .background(configuration.isOn ? .green.opacity(0.4) : .gray.opacity(0.4))
+            .cornerRadius(15)
+            .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color.black, lineWidth: 2)
+                )
+            .onTapGesture {
+                configuration.isOn.toggle()
+            }
+        }
+}
+
+struct ModelIntrectTestView: View {
+    
+    @State var toggle = false
+    
+    var body: some View {
+        VStack{
+            Button("X+"){
+                
+            }
+            .buttonStyle(ModelInteractButton())
+            Toggle("Lock\nROT", isOn: $toggle)
+            .toggleStyle(ModelInteractToggle())
+        }
+    }
+}
+
 struct ModelInteractButton_Previews: PreviewProvider {
     static var previews: some View {
-        Button("X+"){
-            
-        }
-        .buttonStyle(ModelInteractButton())
+        ModelIntrectTestView()
     }
 }
